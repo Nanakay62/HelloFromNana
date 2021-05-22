9136fb677d7f73170cf45498a7209c011866c19f
AnastasiyaShvdko <m.a.34@yandex.ru>
2021-05-22 23:46:32 +0300

    Said Hello

--- src/dickson/HelloFromNana.java
+++ src/dickson/HelloFromNana.java
@@ -3,6 +3,8 @@ package dickson;
 public class HelloFromNana {
     public static void main(String [] args) {
         System.out.println("My name is Nana Kwame Dickson");
+        System.out.println("Hello Nana,my name is Nastya");
+
     }
 
 }
